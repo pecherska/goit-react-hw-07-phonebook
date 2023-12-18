db6446724e361bc1f95316b1bd7b3181e907049d
Nina Pecherska <npecherska@gmail.com>
2023-12-18 23:15:12 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,10 +2,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { FilterContainer, FilterInpur } from './Filter.styled';
 import { filterAction } from 'components/store/filter/filterSlice';
 import { selectFilter } from 'components/store/selectors';
+
 const Filter = () => {
   const filter = useSelector(selectFilter);
   const dispatch = useDispatch();
   const onChange = e => {
+    console.log(e);
     dispatch(filterAction(e.target.value));
   };
   return (
